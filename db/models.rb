6ca7ed0b381bb7cc521cd74275025dfba33e00f0
leila-alderman <leila.alderman@gmail.com>
2019-02-16 19:32:32 -0500

    Add database with seeds

---
+++ db/models.rb
@@ -0,0 +1,6 @@
+require_relative "connection"
+
+class Fish < ActiveRecord::Base
+  
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require_relative "models"
+
+Fish.create!({
+  name: "Goldfish",
+  description: "A great entry fish."
+})
+Fish.create!({ 
+  name: "Tuna",
+  description: "Huge and tasty with mayo."
+})
