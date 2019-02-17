65e9d4054f0eb86b735f394ab907a48e9d546958
leila-alderman <leila.alderman@gmail.com>
2019-02-17 14:15:09 -0500

    Update image links

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,17 @@ Fish.destroy_all
 
 Fish.create([{
   name: "Clownfish",
-  image_url: "http://www.leisurepro.com/blog/wp-content/uploads/2010/04/Ocellaris-Clownfish.jpg",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Clown_fish_in_the_Andaman_Coral_Reef.jpg/640px-Clown_fish_in_the_Andaman_Coral_Reef.jpg",
   description: "This fish isn't very funny."
 },
 { 
   name: "Goldfish",
-  image_url: "http://www.newsworks.org/images/stories/flexicontent/l_shutterstock_goldfish_1200x675.jpg",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Goldfish3.jpg",
   description: "A great entry fish."
 },
 {
   name: "Phish",
-  image_url: "http://bizopy.com/wp-content/uploads/2013/08/phish.jpg",
+  image_url: "https://upload.wikimedia.org/wikipedia/en/e/e9/Whitetape.jpg",
   description: "Known to occasionally grow dreadlocks and smell funny."
 }
 ])
